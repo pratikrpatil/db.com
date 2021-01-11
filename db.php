64664769812960e6858b50105d383f4255975efe
pratikrpatil <47771605+pratikrpatil@users.noreply.github.com>
2021-01-11 18:30:27 +0530

    Add files via upload

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$servername = "localhost:81";
+$username = "root";
+$password = "";
+$db = "login";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
